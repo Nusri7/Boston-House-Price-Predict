3881884e8dbc630fc6faff556e037802470bca90
Nusri7 <321nusri@gmail.com>
2023-08-07 19:32:01 +0530

    Create web application

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,3 +15,11 @@ def home():
 def predict():
     data= request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output= model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
